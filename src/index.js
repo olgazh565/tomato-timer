97eca9ea0f0ab8edba564b9a50c97ee96c57461b
olgazh565 <olgazh565@gmail.com>
2023-10-18 19:35:55 +0300

    eslint added

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import './index.html';
 import './index.scss';
 
 let count = 0;
-const imp = ['default', 'important', 'so-so']
-document.querySelector('.button-importance').addEventListener('click', ({target}) => {
-  count += 1;
-  if (count >= imp.length) {
-    count = 0
-  }
+const imp = ['default', 'important', 'so-so'];
+document.querySelector('.button-importance')
+		.addEventListener('click', ({target}) => {
+			count += 1;
+			if (count >= imp.length) {
+				count = 0;
+			}
 
-  for (let i = 0; i < imp.length; i++) {
-    if (count === i) {
-      target.classList.add(imp[i])
-    } else {
-      target.classList.remove(imp[i])
-    }
-  }
-})
+			for (let i = 0; i < imp.length; i++) {
+				if (count === i) {
+					target.classList.add(imp[i]);
+				} else {
+					target.classList.remove(imp[i]);
+				}
+			}
+		});
