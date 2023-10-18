898ff06c947719ee2842f231623b6c8917821d0c
olgazh565 <olgazh565@gmail.com>
2023-10-18 19:30:17 +0300

    main added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import './index.html';
 import './index.scss';
-import code from './img/code.png'
-import { mult, sum } from './modules/calc';
 
-const imgWrap = document.querySelector('.img');
-const img = new Image();
-img.src = code;
-img.width = 700;
-imgWrap.append(img);
+let count = 0;
+const imp = ['default', 'important', 'so-so']
+document.querySelector('.button-importance').addEventListener('click', ({target}) => {
+  count += 1;
+  if (count >= imp.length) {
+    count = 0
+  }
 
-console.log(mult(3, 4));
-console.log(sum(3, 4));
+  for (let i = 0; i < imp.length; i++) {
+    if (count === i) {
+      target.classList.add(imp[i])
+    } else {
+      target.classList.remove(imp[i])
+    }
+  }
+})
